38bd085a6c9708c286fa5492dfea74c07c0db8cd
McACE007 <mrinalace@gmail.com>
2024-12-01 16:48:15 +0530

    update: hosted frontend on backend server

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "public"))); // Serve static files
+app.use(express.static(path.join(__dirname, "..", "..", "client/dist"))); // Serve static files
 
 const io = new Server(server, {
   cors: {
@@ -270,7 +270,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   // Send the index.html file
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "..", "/client/dist/index.html"));
 });
 
 server.listen(PORT, () => {
